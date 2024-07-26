18300f026948ee91c2e5e59aaa5b4bbd5505b5f6
Rajan Lama <rajan.lama@fusemachines.com>
2024-07-26 12:04:08 +0545

    update github workflow

--- app/app.py
+++ app/app.py
@@ -4,10 +4,12 @@ import redis
 app = Flask(__name__)
 cache = redis.Redis(host='redis', port=6379)
 
+
 @app.route('/')
 def hello_world():
     count = cache.incr('hits')
-    return f'Hello World! This page has been visited {count} times.'
+    return f'Hello World! This page has been visited times.'
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
---
+++ app/tests/test_app.py
@@ -0,0 +1,5 @@
+from app import index
+
+
+def test_index():
+    assert index() == "Hello World! This page has been visited times."
